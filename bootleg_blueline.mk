89c83e4330b446f2de00143a7e8d016dadb65e03
stebomurkn420 <stevn4127@gmail.com>
2019-03-11 05:45:02 -0400

    Update March Build ID, Lets make this Official, add myself as maintainer so people know who to blame..

--- bootleg_blueline.mk
+++ bootleg_blueline.mk
@@ -43,8 +43,14 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=blueline \
-    BUILD_FINGERPRINT=google/blueline/blueline:9/PQ1A.190105.004/5148680:user/release-keys \
-    PRIVATE_BUILD_DESC="blueline-user 9 PQ1A.190105.004 5148680 release-keys"
+    BUILD_FINGERPRINT=google/blueline/blueline:9/PQ2A.190305.002/5240760:user/release-keys \
+    PRIVATE_BUILD_DESC="blueline-user 9 PQ2A.190305.002 5240760 release-keys"
+
+# Shishufied builds
+BOOTLEGGERS_BUILD_TYPE := Shishufied
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bootleggers.maintainer=stebomurkn420
 
 $(call inherit-product-if-exists, vendor/google/blueline/blueline-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
