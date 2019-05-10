0beffb7edcbee0a23d52e7053678b84d0f0fc78a
stebomurkn420 <stevn4127@gmail.com>
2019-05-10 02:07:20 -0400

    May build fingerprint

--- bootleg_blueline.mk
+++ bootleg_blueline.mk
@@ -43,8 +43,8 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=blueline \
-    BUILD_FINGERPRINT=google/blueline/blueline:9/PQ2A.190405.003/5310204:user/release-keys \
-    PRIVATE_BUILD_DESC="blueline-user 9 PQ2A.190405.003 5310204 release-keys"
+    BUILD_FINGERPRINT=google/blueline/blueline:9/PQ3A.190505.002/5450365:user/release-keys \
+    PRIVATE_BUILD_DESC="blueline-user 9 PQ3A.190505.002 5450365 release-keys"
 
 # Shishufied builds
 BOOTLEGGERS_BUILD_TYPE := Shishufied
